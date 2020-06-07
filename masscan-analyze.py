f1c2cb54a4f52077db6a94622bc25013318c1c0e
Dillon Reece Franke <dfranke@stanford.edu>
2020-06-07 16:41:10 -0700

    adding analysis scripts for json files

---
+++ masscan-analyze.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import json
+import collections
+
+
+scan_map = {}
+
+count = 0
+
+def ppjson(obj):
+    print(json.dumps(obj, indent=2))
+
+if __name__ == "__main__":
+    # Country analysis
+    f = open('go/src/pcap/masscan.json', 'r')
+    countries = json.load(f)
+
+    for entry in countries:
+        srcip = entry['SrcIp']
+        ports = entry['Ports']
+
+        for port in ports:
+            if port['Hits'] >= 40:
+                count += 1
+
+    print(count)
